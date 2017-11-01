119f09802ca3cc3c64a32d2f154825369e8dc3db
cyssxt <cyssxt@163.com>
2017-11-01 21:09:09 +0800

    add demo

--- src/test/java/com/cyssxt/test.java
+++ src/test/java/com/cyssxt/test.java
@@ -4,6 +4,9 @@ import com.cyssxt.manager.ThreadManager;
 import com.cyssxt.task.AbstractTask;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * author:cyssxt
  * date:2017/11/1
@@ -17,21 +20,29 @@ public class test {
     public static void test(){
 
         AbstractTask abstractTask = new AbstractTask(){
-
-
-
+            private int run_time = 10;
             public void run() {
                 System.out.println("asdasd");
+            }
+
+            public boolean filter(){
+                return run_time-->0;
+            }
+        };
+        AbstractTask abstractTask1 = new AbstractTask(){
+            public void run() {
+                System.out.println("asdasd1");
             }
 
 //            public boolean filter(){
 //                return true;
 //            }
-
         };
-        ThreadManager threadManager = ThreadManager.getIntance(3,abstractTask);
+        List<AbstractTask> list = new ArrayList<AbstractTask>();
+        list.add(abstractTask);
+        list.add(abstractTask1);
+        ThreadManager threadManager = ThreadManager.getIntance(1,list);
         threadManager.start();
-
     }
 
     public static void main(String[] args) {
