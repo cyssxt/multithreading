c69bf3e27e64bff7ec6de9b30fb6f06853d0efd7
cyssxt <cyssxt@163.com>
2017-11-01 21:04:23 +0800

    add log

--- src/main/java/com/cyssxt/thread/core/CoreThread.java
+++ src/main/java/com/cyssxt/thread/core/CoreThread.java
@@ -2,21 +2,26 @@ package com.cyssxt.thread.core;
 
 import com.cyssxt.manager.ThreadManager;
 import com.cyssxt.task.AbstractTask;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Date;
 import java.util.List;
 
 public class CoreThread extends Thread{
 
+    private final Logger logger = LoggerFactory.getLogger(CoreThread.class);
     private ThreadManager threadManager;
     private int index;
 
     public CoreThread(ThreadManager threadManager,int index){
+        logger.info("CoreThread=>init:index:{}",index);
         this.threadManager = threadManager;
         this.index = index;
     }
 
     @Override
     public void run() {
+        logger.info("CoreThread=>run:index:{},date={}",index,new Date());
         List<AbstractTask> abstractTaskList  = threadManager.getThreadConfig().getAbstractTaskList();
         AbstractTask abstractTask = abstractTaskList.get(index);
         this.threadManager.start(abstractTask);
@@ -24,6 +29,7 @@ public class CoreThread extends Thread{
         while(abstractTask.filter()){
             abstractTask.run();
         }
+        logger.info("CoreThread=>run:end:{},date={}",index,new Date());
         abstractTask.end();
         this.threadManager.end(abstractTask);
     }
